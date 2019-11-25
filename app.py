6bea976073a3ab0459794226082146df61aafc0d
biwalsh72 <biwalsh72@gmail.com>
2019-11-25 02:13:53 -0500

    UI changes + setting up for data
    working on scalability next

--- app.py
+++ app.py
@@ -10,78 +10,16 @@ import asyncio
 from lib.cortex import Cortex
 from flask import Flask, request, make_response
 
+# global array to store ['pow'] values
+power = []
+
+# x-axis of graph of
+time = deque(maxlen=10)
+time.append(1)
+
 external_stylesheets = [{'href': "https://fonts.googleapis.com/css?family=Roboto:300&display=swap",
                          'rel': "stylesheet"},
                         ]
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-app.layout = html.Div(className='container', children=[
-
-    html.Div(id="head", children=(html.H2(
-        id="header", children='EMOTIV PERFORMANCE METRICS'), dcc.Dropdown(id='menu',
-                                                                          options=[
-                                                                              {'label': 'Channel: AF3',
-                                                                                  'value': 'AF3'},
-                                                                              {'label': 'Channel: AF4',
-                                                                                  'value': 'AF4'},
-                                                                          ],
-                                                                          value='AF3'
-                                                                          ))),
-
-    # dcc.Interval(
-    #    id='graph-pow-update',
-    #    interval=1000,
-    #    n_intervals=0
-    # ),
-
-    html.Div(id='stats', children=(html.H3(className='stats-text', children='Engagement '),
-                                   html.H3(className='stats-text', children='Fatigue'), html.H4())),
-
-    html.Div(id='line-graph', children=dcc.Graph(id='live-pow-line',
-                                                 animate=True, figure={'data': [
-                                                     {'x': [0, 2, 3], 'y':[
-                                                         1, 2, 3], 'name':'Low Beta', 'type':'line'},
-                                                     {'x': [1, 2, 8], 'y':[
-                                                         1, 3, 4], 'name':'High Beta', 'type':'line'},
-                                                     {'x': [2, 3, 5], 'y':[
-                                                         1, 2, 3], 'name':'Alpha', 'type':'line'},
-                                                     {'x': [1, 5, 6], 'y':[
-                                                         1, 7, 9], 'name':'Theta', 'type':'line'},
-                                                 ], 'layout': dict(plot_bgcolor='#ffffff', paper_bgcolor='#dddddd', autosize=True, title='Band Power',
-                                                                   xaxis=dict(automargin=True, title=dict(
-                                                                       text='Time', font=dict(size=30))),
-                                                                   yaxis=dict(automargin=True, title=dict(
-                                                                       text='AF3', font=dict(size=30))),
-                                                                   margin=dict(l=45, r=250, t=50, b=40), )})),
-]
-)
-
-
-@app.callback(
-    Output(component_id='live-pow-line', component_property='figure'),
-    [Input(component_id='menu', component_property='value')]
-)
-def update_graphChannel(channel):
-    if channel == 'AF3':
-        yaxiss = 'AF4'
-    else:
-        yaxiss = 'AF3'
-
-    return {'data': [{'x': [0, 2, 3], 'y':[1, 2, 3], 'name':'Low Beta', 'type':'line'},
-                                                     {'x': [1, 2, 8], 'y':[
-                                                         1, 3, 4], 'name':'High Beta', 'type':'line'},
-                                                     {'x': [2, 3, 5], 'y':[
-                                                         1, 2, 3], 'name':'Alpha', 'type':'line'},
-                                                     {'x': [1, 5, 6], 'y':[
-                                                         1, 7, 9], 'name':'Theta', 'type':'line'},
-                                                 ], 'layout': dict(plot_bgcolor='#ffffff', paper_bgcolor='#dddddd', autosize=True, title='Band Power',
-                                                                   xaxis=dict(automargin=True, title=dict(
-                                                                       text='Time', font=dict(size=30))),
-                                                                   yaxis=dict(automargin=True, title=dict(
-                                                                       text=yaxiss, font=dict(size=30))),
-                                                                   margin=dict(l=45, r=250, t=50, b=40), )}
-
 
 
 async def authorize(cortex):
@@ -109,8 +47,8 @@ async def authorize(cortex):
         print("** SUBSCRIBE TO POW **")
         await cortex.subscribe(['pow'])
         # put pow stream into power variable
-        power=['pow']
-        print(*power)
+        power = ['pow']
+        print(power)
         # print pow list
 
         while cortex.packet_count < 10:
@@ -123,10 +61,85 @@ async def authorize(cortex):
 
 
 def cortexService():
-    cortex=Cortex('./cortex_creds')
+    cortex = Cortex('./cortex_creds')
     asyncio.run(authorize(cortex))
     cortex.close()
 
+
+data_set = {
+    'band-power': {
+        'data': [
+
+        ]
+    },
+    'perf-metrics': {
+        'data': [
+
+        ]
+    }
+}
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div(className='container', children=[
+
+    html.Div(id="head", children=(html.H2(
+        id="header", children='EMOTIV PERFORMANCE METRICS'),
+        dcc.Dropdown(id='menu',
+                     options=[
+                         {'label': 'Channel: AF3',
+                          'value': 'AF3'},
+                         {'label': 'Channel: AF4',
+                          'value': 'AF4'},
+                     ],
+                     value='AF3'
+                     ))),
+
+    html.Div(id='stats', children=(html.H3(className='stats-text', children='Engagement '),
+                                   html.H3(className='stats-text', children='Fatigue'), html.H4())),
+
+    html.Div(id='line-graph', children=[dcc.Graph(id='live-pow-line'),
+                                        dcc.Interval(
+        id='graph-update', interval=1*1000, n_intervals=0
+    )
+    ]
+    )
+]
+)
+
+
+@app.callback(
+    Output('live-pow-line', 'figure'),
+    [Input('menu', 'value')]
+)
+def update_graphChannel(channel):
+    return {'layout': dict(plot_bgcolor='#ffffff', paper_bgcolor='#dddddd', autosize=True, title='Band Power for channel ' + channel,
+                           xaxis=dict(automargin=True, title=dict(
+                               text='Time', font=dict(size=30))),
+                           margin=dict(l=45, r=250, t=50, b=40), )}
+
+
+'''
+@app.callback(
+    Output(component_id='live-pow-line', component_property='figure'),
+    [Input(component_id='graph-update', component_property='interval')]
+)
+def updateGraph():
+    val = 0
+    global time
+    time.append(time[-1]+1)
+    val.append(power[-1]+1)
+
+    data = go.Scatter(
+        x=list(time),
+        y=list(val),
+        name='scatter',
+        mode='lines+markers'
+    )
+
+    return {'data': [data], 'layout': go.Layout(xaxis=dict(range=[min(time), max(time)]),
+                                                yaxis=dict(range=[min(val), max(val)]))}
+'''
 
 # run test function when server starts
 # cortexService()
